390ef8623b65be912e4ecb7c5a3bed6dd91728af
LS.551 <medinajoseluis412@gmail.com>
2023-04-13 13:56:31 -0400

    news files

--- my-app/src/Button.js
+++
@@ -1,9 +0,0 @@
-export function Button({text}) {
-    console.log(text);
-    return <button onClick={function() {
-        console.log('hola mundo')
-    }}>
-        {text}
-    </button>
-}
-  
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,40 +1,13 @@
-import React,{useState,useEffect} from 'react';
-import ReactDOM from 'react-dom/client';
-import {Greeting, Usercard} from './Greeting';
-import Product,{Navbar} from './Product'
-import {Button} from  './Button'
-import {TaskCard} from './Task'
-import {Saludar} from './Saludar'
-import {Post} from './post'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './app'
+import {TaskContextProvider} from './context/TaskContext'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-function Contar() {
-    
-   const [ mensaje,setMensaje ] = useState('');
-   const [counter,setCounter] = useState(0)
-
-   useEffect(() => {
-    console.log('render')
-   },[counter])
-
-    return(
-        <div>
-            <input onChange={e => setMensaje(e.target.value)}/>
-            <button onClick={() => {alert('el mensaje es: ' + mensaje);}}>Guardar</button>
-        <hr/>
-
-        <h1>Counter: {counter}</h1>
-        <button onClick={() => setCounter(counter + 1)}>Incrementar</button>
-        </div>
-    )
-}
-
-
-root.render(<>
-
-  <>
-   <Contar/>
-  </>
-
-</>)
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <TaskContextProvider>
+           <App />
+        </TaskContextProvider>
+    </React.StrictMode>
+)
